8d61b02e778d5eeb9837a7cc48014a87b8301792
Samuel Sekiwere <sekiskylink@gmail.com>
2016-12-03 09:11:26 +0300

    add reporter code to registration_forms_view

--- schema/llin.sql
+++ schema/llin.sql
@@ -635,7 +635,7 @@ CREATE VIEW reporters_view2 AS
 DROP VIEW IF EXISTS registration_forms_view;
 CREATE VIEW registration_forms_view AS
     SELECT
-        a.firstname, a.lastname, a.email, a.uuid, a.telephone, a.alternate_tel,
+        a.firstname, a.lastname, a.email, a.uuid, a.telephone, a.alternate_tel, a.code as reporter_code,
         b.serial_number as form_serial, to_char(b.created, 'YYYY-MM-DD HH24:MI') as created,
         b.created as cdate, c.name as location_name, c.code as location_code,
         c.uuid as location_uuid, c.id as location_id
