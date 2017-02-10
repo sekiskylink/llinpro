c74105b380efd62c7b3f233a1f70fca67840db41
Samuel Sekiwere <sekiskylink@gmail.com>
2017-02-10 21:09:17 +0300

    make sure cvs for locations can be accessed via the api

--- web/app/controllers/api2.py
+++ web/app/controllers/api2.py
@@ -1,8 +1,8 @@
 import json
 from . import db
 import web
-from settings import config
-import os
+# from settings import config
+# import os
 from app.tools.utils import get_basic_auth_credentials, auth_user
 
 
@@ -58,11 +58,11 @@ class LocationsCSVEndpoint:
             return json.dumps({'detail': 'Authentication failed!'})
         y = db.query("COPY(SELECT * FROM locations) to '/tmp/llin.csv'  with delimiter ',' csv header;")
         if y:
-            static_directory = config.get('static_directory', '/var/www/llinpro/web/static')
-            os.popen("zip /tmp/llin.csv.zip /tmp/llin.csv; cp /tmp/llin.csv.zip %s" % static_directory)
+            # static_directory = config.get('static_directory', '/var/www/llinpro/web/static')
+            # os.popen("zip /tmp/llin.csv.zip /tmp/llin.csv; cp /tmp/llin.csv.zip %s" % static_directory)
             web.header("Content-Type", "application/zip; charset=utf-8")
             # web.header('Content-disposition', 'attachment; filename=%s.csv'%file_name)
-            web.seeother("/static/llin.csv.zip")
+            web.seeother("/static/downloads/llin.csv.zip")
         else:
             web.header("Content-Type", "application/json; charset=utf-8")
             return json.dumps({'detail': 'CSV download service failed!'})
